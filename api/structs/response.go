754135421708905f77adf94f7760c572b85221bc
jan <git@wiegelmann.net>
2025-03-22 14:41:05 +0100

    bump dependencies

--- api/structs/response.go
+++ api/structs/response.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"strings"
 
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
